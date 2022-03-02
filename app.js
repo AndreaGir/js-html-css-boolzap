0ea4e3f104d84a700332240e7135d9188fcf2f04
AndreaGir <girolamiandrea2000@gmail.com>
2022-03-02 20:43:45 +0100

    ...

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const app = new Vue(
+ const app = new Vue(
     {
         el: '#tutto',
         data:{
@@ -7,19 +7,28 @@ const app = new Vue(
                     nome: 'Andrea',
                     messaggio: 'Ci vediamo dopo?',
                     img: 'img/img3.jfif', 
+                    ora: '11:27',
                 },
                 {
                     nome: 'Francesco',
                     messaggio: 'Ciaoo',
                     img: 'img/img4.jfif', 
+                    ora: '10:00',
                 },
                 {
                     nome: 'Marco',
                     messaggio: 'come stai?',
                     img: 'img/img2.jfif', 
+                    ora: '1:32',
+                },
+                {
+                    nome: 'federico',
+                    messaggio: 'che fai?',
+                    img: 'img/img2.jfif', 
+                    ora: '1:32',
                 },
                 
             ]
         }
     }
-)
+) 
